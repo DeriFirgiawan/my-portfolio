b774c7f83f9ee2b3da7b0f98629c71f9874a2605
derifirgiawan <derifirgiawan025@gmail.com>
2024-01-13 17:53:25 +0700

    fix: props

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ const jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Deri Firgiawan",
-	description: "Portfolio Deri Firgiawan",
+	description:
+		"As a junior software developer at Xcidic, I design and develop user-friendly web applications using Redux Thunk, Tailwind CSS, and Node.js. I have two years of work experience in frontend engineering, including six months at Sahaware Teknologi Indonesia, where I contributed to the development of a cloud-based e-commerce platform. I have also earned multiple certifications from Dicoding Indonesia, a leading online learning platform, in Dart, Web Development, and Front-End Web Development. I graduated with an associate's degree in informatika from SMK Merdeka Bandung in 2021, where I learned the fundamentals of programming, data structures, algorithms, and databases. I am passionate about creating innovative and accessible web solutions that solve real-world problems and enhance user experience. I am always eager to learn new technologies and best practices, and to collaborate with other developers and stakeholders. My goal is to become a senior software developer and a mentor for aspiring programmers.",
 };
 
 export default function RootLayout({
